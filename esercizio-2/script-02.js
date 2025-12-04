b5fb69945d46a6dcef332f70557806f85a1e43f3
Salvatore Scalise <salvatorescalise1999@gmail.com>
2025-12-04 12:29:34 +0100

    feat: versione con ciclo while

--- esercizio-2/script-02.js
+++ esercizio-2/script-02.js
@@ -4,19 +4,41 @@
 //  Esegui questo programma in due versioni, con il for e con il while (facoltativo)
 
 
-let somma = 0;
+//  ciclo for
 
-for (let i = 1; i <= 5; i++) {
-  let numero = parseFloat(prompt("Inserisci il numero " + i + ":"));
+// let somma = 0;
+
+// for (let i = 1; i <= 5; i++) {
+//   let numero = parseFloat(prompt("Inserisci il numero " + i + ":"));
+
+//   if (isNaN(numero)) {
+//     alert("Non hai inserito un numero valido! Riprova.");
+//     i--; // torna indietro e richiedi lo stesso numero
+//   } else {
+//     somma += numero;
+//   }
+// }
+
+// console.log("La somma è:", somma);
+// alert("La somma dei numeri inseriti è: " + somma);
+
+
+// ciclo while
+
+let somma = 0;
+let contatore = 1;
+
+while (contatore <= 5) {
+  let numero = parseFloat(prompt("Inserisci il numero " + contatore + ":"));
 
   if (isNaN(numero)) {
-    alert("Non hai inserito un numero valido! Riprova.");
-    i--; // torna indietro e richiedi lo stesso numero
+    alert("Errore! Devi inserire un numero.");
   } else {
     somma += numero;
+    contatore++; // incremento solo se l’input è valido
   }
 }
 
 console.log("La somma è:", somma);
-alert("La somma dei numeri inseriti è: " + somma);
+alert("La somma totale è: " + somma);
 
